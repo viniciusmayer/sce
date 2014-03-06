16c45ed8d4ef9ec8a1fd576fa89b5d7dd5c917fa
Vinicius Mayer <viniciusmayer@gmail.com>
2014-03-06 17:31:46 -0300

    Initial commit.

---
+++ sce/src/br/com/ftec/poo/swing/Usuario.java
@@ -0,0 +1,46 @@
+package br.com.ftec.poo.swing;
+
+public class Usuario {
+    
+    private Long id;
+    private String email;
+    private String senha;
+
+    public Usuario(){
+    }
+    
+    public Usuario(String email, String senha){
+        this.email = email;
+        this.senha = senha;
+    }
+    
+    public Usuario(Long id, String email, String senha){
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
